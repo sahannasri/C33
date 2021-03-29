48aebfc35fc6773e270f7b3ae3d22d35e9de2b11
sahannasri@gmail.com <sahannasri@gmail.com>
2021-03-29 21:25:05 +1100

    add

--- Pig.js
+++ Pig.js
@@ -2,6 +2,7 @@ class Pig extends BaseClass {
   constructor(x, y){
     super(x,y,50,50);
     this.image = loadImage("sprites/enemy.png");
+    this.remove = true;
  //this.Visiblity=255;
   }
 display()
@@ -12,6 +13,10 @@ if((this.body.speed)<2.5)
 }else
 {
 World.remove(world,this.body)  
+if (this.remove==true){
+  score = score + 50;
+  this.remove = false;
+}
 
 //push();
 //this.Visiblity=this.Visiblity-5;
